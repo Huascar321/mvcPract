7d297efb6d078c1566417f00540f55d9eb1ae13f
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 16:25:08 -0400

    Segundo commit

---
+++ mvcPract/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvcPract.MvcApplication" Language="C#" %>
---
+++ mvcPract/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace mvcPract.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public enum TypeContact
+    {
+        telephone,
+        email,
+        facebook,
+        twitter,
+        instagram,
+        dblink
+    }
+    public class Book
+    {
+        [Key]
+        public int bookID { get; set; }
+        [Required] //Significa que te volvera a pedir datos hasta que pongas algo
+        public string name { get; set; }
+        [Required] 
+        public TypeContact Type { get; set; }
+        [Required]
+        public string contact { get; set; }
+    }
+}
